9ca0fde420ea8b7e4ff066c8d97088ff44b55e62
Md Mohosin Islam Rijon <mdrijonmia1271@gmaiil.com>
2022-09-24 23:43:14 +0600

    Return Square

---
+++ returningValue/ReturnSquare.java
@@ -0,0 +1,8 @@
+package returningValue;
+
+public class ReturnSquare {
+    
+    int square (int value){
+        return value*value;
+    }
+}
---
+++ returningValue/Test.java
@@ -0,0 +1,10 @@
+package returningValue;
+
+public class Test {
+    public static void main(String[] args) {
+        
+
+        ReturnSquare obj = new ReturnSquare();
+        System.out.println("Square 5 = "+obj.square(5));
+    }
+}
